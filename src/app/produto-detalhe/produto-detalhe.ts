ad40a587fb12f4b0ad5080a5730de98b1686cf21
Bruno Emerson Gurgel Gomes <1582311@ifrn.local>
2026-09-01 16:11:44 -0300

    Exemplo atualizado com recebimento do parâmetro e chamada ao método para obter produto

--- src/app/produto-detalhe/produto-detalhe.ts
+++ src/app/produto-detalhe/produto-detalhe.ts
@@ -1,4 +1,6 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input, OnInit, signal } from '@angular/core';
+import { LojaService } from '../loja-service';
+import { Produto } from '../produto';
 
 @Component({
   imports: [],
@@ -6,8 +8,19 @@ import { Component, input } from '@angular/core';
   styleUrl: './produto-detalhe.scss',
   templateUrl: './produto-detalhe.html',
 })
-export class ProdutoDetalhe {
+export class ProdutoDetalhe implements OnInit {
   id = input.required<number>();
+  produto = signal<Produto | undefined>(undefined)
+ 
+  readonly #lojaService = inject(LojaService)
 
-  
+  ngOnInit(): void {
+    this.detalharProduto()
+  }
+
+  detalharProduto() {
+    this.#lojaService.obterProdutoPorId(this.id()).subscribe(prod => {
+      this.produto.set(prod)
+    })
+  }
 }
